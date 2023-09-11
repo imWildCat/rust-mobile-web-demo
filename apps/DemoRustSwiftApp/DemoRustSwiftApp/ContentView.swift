6d1a1cbf1e583b7ad8d7c169153cf2c81a4a06f8
Daohan Chong <dachon@microsoft.com>
2023-09-11 15:32:04 -0700

    init swift project

---
+++ apps/DemoRustSwiftApp/DemoRustSwiftApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  DemoRustSwiftApp
+//
+//  Created by Daohan Chong on 9/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ apps/DemoRustSwiftApp/DemoRustSwiftApp/DemoRustSwiftAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoRustSwiftAppApp.swift
+//  DemoRustSwiftApp
+//
+//  Created by Daohan Chong on 9/11/23.
+//
+
+import SwiftUI
+
+@main
+struct DemoRustSwiftAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
